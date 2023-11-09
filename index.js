ec931a63c38eef601785f55cc5814528f57119f0
Gorgees Odisho <gorgees041@gmail.com>
2023-11-09 05:35:40 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+// Breakfast class
+class Breakfast {
+  constructor(food, drink) {
+    (this.food = food), (this.drink = drink);
+  }
+}
+
+// Lunch class
+class Lunch {
+  constructor(salad, soup, drink) {
+    (this.salad = salad), (this.soup = soup), (this.drink = drink);
+  }
+}
+
+// dinner class
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    (this.salad = salad),
+      (this.soup = soup),
+      (this.entree = entree),
+      (this.#dessert = dessert);
+  }
+}
